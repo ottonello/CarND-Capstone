fbadc609027163aa9b37ae1add23ff37c238485f
Marcos Ottonello <jmottonello@gmail.com>
2017-12-23 13:33:50 -0800

    Fix: add lpf to twist controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,6 +1,8 @@
 from pid import PID
 from yaw_controller import YawController
+from lowpass import LowPassFilter
 import math
+
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
@@ -13,6 +15,8 @@ class Controller(object):
         self.linear_pid = PID(kp=0.8, ki=0, kd=0.05, mn=self.params.decel_limit, mx=0.5 * self.params.accel_limit)
         self.yaw_controller = YawController(self.params.wheel_base, self.params.steer_ratio, min_speed, self.params.max_lat_accel, self.params.max_steer_angle)
         self.steering_pid = PID(kp=0.15, ki=0.001, kd=0.1, mn=-self.params.max_steer_angle, mx=self.params.max_steer_angle)
+        self.throttle_lpf = LowPassFilter(tau = 3, ts = 1)
+        self.steer_lpf = LowPassFilter(tau = 3, ts = 1)
 
     def reset(self):
         self.linear_pid.reset()
@@ -23,6 +27,7 @@ class Controller(object):
 
         brake = 0
         throttle = self.linear_pid.step(linear_velocity_error, duration_in_seconds)
+        throttle = self.throttle_lpf.filt(throttle)
 
         if(throttle < 0):
             deceleration = abs(throttle)
@@ -33,5 +38,6 @@ class Controller(object):
         predictive_steering = self.yaw_controller.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
         corrective_steering = self.steering_pid.step(cross_track_error, duration_in_seconds)
         steering = predictive_steering + corrective_steering
+        steering = self.steer_lpf.filt(steering)
 
         return throttle, brake, steering
